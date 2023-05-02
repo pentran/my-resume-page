f07aec7348d08e370250571bbff56f16bf774ebb
pentran <penny.tran@stonybrook.edu>
2023-05-02 07:36:58 +0000

    have added new html

--- app.py
+++ app.py
@@ -4,5 +4,5 @@ app = Flask(__name__)
 def hello_world():
     return render_template('home.html')
 
-# app.run(host='0.0.0.0', port=5000, debug=True)
+#app.run(host='0.0.0.0', port=5000, debug=True)
 
